87322d9ac8e66686874f02ad857657cadf7db849
詹聪聪 <1227753320@qq.com>
2017-08-28 01:37:10 +0800

    initial

---
+++ mysite.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/websites')
+def websites():
+    return render_template('websites.html')
+
+
+@app.route('/activities', methods=['GET'])
+def activities():
+    return render_template('activities.html')
+
+
+@app.route('/translate', methods=['GET', 'POST'])
+def translate():
+    return render_template('translate.html')
+
+
+@app.route('/activities/start', methods=['POST'])
+def start_activities():
+    return render_template('output.html')
+
+
+@app.route('/translate/sdata', methods=['POST'])
+def translate_sdata():
+
+    return render_template('output.html')
+
+
+@app.route('/translate/properties', methods=['POST'])
+def translate_properties():
+    url = 'http://127.0.0.1:65534/static/img/avatar-lady.jpg'
+    return render_template('output.html', url=url)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=65534)
